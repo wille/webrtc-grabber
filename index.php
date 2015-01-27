382f19f9982e8858dcca902bd6eb4024ede89037
redpois0n <git@redpois0n>
2015-01-27 16:59:26 +0100

    Works, now make it hidden and automatic

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php
+
+	echo count($_POST);
+	var_dump($_POST);
+	//exit();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+    	<form id="f" method="POST">
+    	<input name="test" value="test" type="hidden">
+    	<input type=submit name="submit" id="submit" value="Continue"/>
+    	</form>
+        <h4>Your local IP addresses:</h4>
+        <ul></ul>
+        <h4>Your public IP addresses:</h4>
+        <ul></ul>
+        <script src="webrtc.js"></script>
+        <script>
+			//document.getElementById("f").submit();
+        </script>
+    </body>
+</html>

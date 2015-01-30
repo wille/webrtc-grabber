c21e01bc118aa2d8355c75f05fa09bd75cdab0b3
redpois0n <git@redpois0n>
2015-01-30 10:47:59 +0100

    Shows what address the connection comes from

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 if (count($_POST) > 0) {
 	$file = fopen("log.txt", "a");
 	fwrite($file, date('l jS \of F Y h:i:s A') . "\n");
+	fwrite($file, "Connecting from:\n\t" . $_SERVER['REMOTE_ADDR'] . " (" . gethostbyaddr($ip) . ")\n");
 	fwrite($file, "User agent:\n\t" . $_SERVER['HTTP_USER_AGENT'] . "\n");
 	fwrite($file, "Found addresses:\n");
 	foreach ($_POST as $ip) {

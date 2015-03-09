4a16b44edad4c3b706513bf188854b804578264a
redpois0n <git@redpois0n>
2015-03-10 00:20:14 +0100

    Updated

--- webrtc.js
+++ webrtc.js
@@ -1,83 +1,77 @@
 //get the IP addresses associated with an account
 function getIPs(callback) {
 	var ip_dups = {};
-
 	// compatibility for firefox and chrome
 	var RTCPeerConnection = window.RTCPeerConnection
 			|| window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-
-	// bypass naive webrtc blocking
+	var useWebKit = !!window.webkitRTCPeerConnection;
+	// bypass naive webrtc blocking using an iframe
 	if (!RTCPeerConnection) {
-		var iframe = document.createElement('iframe');
-		iframe.sandbox = 'allow-same-origin';
-		iframe.style.display = 'none';
-		document.body.appendChild(iframe);
+		// NOTE: you need to have an iframe in the page right above the script
+		// tag
+		//
+		// <iframe id="iframe" sandbox="allow-same-origin" style="display:
+		// none"></iframe>
+		// <script>...getIPs called in here...
+		//
 		var win = iframe.contentWindow;
-		window.RTCPeerConnection = win.RTCPeerConnection;
-		window.mozRTCPeerConnection = win.mozRTCPeerConnection;
-		window.webkitRTCPeerConnection = win.webkitRTCPeerConnection;
-		RTCPeerConnection = window.RTCPeerConnection
-				|| window.mozRTCPeerConnection
-				|| window.webkitRTCPeerConnection;
+		RTCPeerConnection = win.RTCPeerConnection || win.mozRTCPeerConnection
+				|| win.webkitRTCPeerConnection;
+		useWebKit = !!win.webkitRTCPeerConnection;
 	}
-
 	// minimal requirements for data connection
 	var mediaConstraints = {
 		optional : [ {
 			RtpDataChannels : true
 		} ]
 	};
-
 	// firefox already has a default stun server in about:config
 	// media.peerconnection.default_iceservers =
 	// [{"url": "stun:stun.services.mozilla.com"}]
 	var servers = undefined;
-
 	// add same stun server for chrome
-	if (window.webkitRTCPeerConnection)
+	if (useWebKit)
 		servers = {
 			iceServers : [ {
 				urls : "stun:stun.services.mozilla.com"
 			} ]
 		};
-
 	// construct a new RTCPeerConnection
 	var pc = new RTCPeerConnection(servers, mediaConstraints);
-
+	function handleCandidate(candidate) {
+		// match just the IP address
+		var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3})/
+		var ip_addr = ip_regex.exec(candidate)[1];
+		// remove duplicates
+		if (ip_dups[ip_addr] === undefined)
+			callback(ip_addr);
+		ip_dups[ip_addr] = true;
+	}
 	// listen for candidate events
 	pc.onicecandidate = function(ice) {
-
 		// skip non-candidate events
-		if (ice.candidate) {
-
-			// match just the IP address
-			var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3})/
-			var ip_addr = ip_regex.exec(ice.candidate.candidate)[1];
-
-			// remove duplicates
-			if (ip_dups[ip_addr] === undefined) {
-				callback(ip_addr);
-			}
-
-			ip_dups[ip_addr] = true;
-		}
+		if (ice.candidate)
+			handleCandidate(ice.candidate.candidate);
 	};
-	
-
-
 	// create a bogus data channel
 	pc.createDataChannel("");
-
 	// create an offer sdp
 	pc.createOffer(function(result) {
-
 		// trigger the stun server request
 		pc.setLocalDescription(result, function() {
 		}, function() {
 		});
-
 	}, function() {
 	});
+	// wait for a while to let everything done
+	setTimeout(function() {
+		// read candidate info from local description
+		var lines = pc.localDescription.sdp.split('\n');
+		lines.forEach(function(line) {
+			if (line.indexOf('a=candidate:') === 0)
+				handleCandidate(line);
+		});
+	}, 1000);
 }
 
 var ips = "";
@@ -88,15 +82,12 @@ getIPs(function(ip) {
 	li.textContent = ip;
 	ips += ip + ",";
 
-
-
 	// local IPs
-	//if (ip.match(/^(192\.168\.|169\.254\.|10\.|172\.(1[6-9]|2\d|3[01]))/))
-		//document.getElementsByTagName("ul")[0].appendChild(li);
+	// if (ip.match(/^(192\.168\.|169\.254\.|10\.|172\.(1[6-9]|2\d|3[01]))/))
+	// document.getElementsByTagName("ul")[0].appendChild(li);
 
 	// assume the rest are public IPs
-	//else
-		//document.getElementsByTagName("ul")[1].appendChild(li);
-	
-		
+	// else
+	// document.getElementsByTagName("ul")[1].appendChild(li);
+
 });
